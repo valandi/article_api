0aa58cd50206d771a621497fa9f40696bb6fb17e
Chris Allulis <valandi.allulis@gmail.com>
2021-04-20 18:13:25 -0700

    Set up babel config, have server respond back, build out controller/model/routes scaffolding

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
-import express from 'express';
+import express, { response } from 'express';
 
 const app = express();
 
 const PORT = 2814;
+
+app.get('/', (request, response) => 
+    response.send(`Server running on port ${PORT}`)
+)
 
 app.listen(PORT, () =>
     console.log(`Nice! Your server is running on port ${PORT}`)
